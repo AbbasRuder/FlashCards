90398582a5013e2bb14fe954fce31e42714ed85d
AbbasRuder <59762007+AbbasRuder@users.noreply.github.com>
2023-05-16 20:36:16 +0530

    Added dynamic height setting for flashcards

--- src/Components/FlashCard.jsx
+++ src/Components/FlashCard.jsx
@@ -1,14 +1,35 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 export default function FlashCard({flashcard}) {
     const[flip, setFlip] = useState(false)
+    const[height, setHeight] = useState('initial')
 
-  
+    const frontRef = useRef();
+    const backRef = useRef();
+
+    function setMaxHeight() {
+        const frontHeight = frontRef.current.getBoundingClientRect().height;
+        const backHeight = backRef.current.getBoundingClientRect().height;
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(
+        setMaxHeight,
+        [flashcard.answer, flashcard.question, flashcard.options]
+    )
+
+    useEffect(() => {
+        // everytime the page resizes, setMaxHeight is called.
+        window.addEventListener('resize', setMaxHeight);
+        return () => removeEventListener('resize', setMaxHeight)
+    }, [])
+
     return (
         <>
             {/* CARD */}
-           <div className = {`card ${flip ? 'flip' : ''}`} onClick={() => {setFlip(!flip)}}>
+           <div className = {`card ${flip ? 'flip' : ''}`} onClick={() => {setFlip(!flip)}}
+                style={{height: height}}>
                 {/* Front card */}
-                <div className="front">
+                <div className="front" ref={frontRef}>
                     {flashcard.question}
                     <div className="flashcard-options">
                         {flashcard.options.map(option => {
@@ -17,7 +38,7 @@ export default function FlashCard({flashcard}) {
                     </div>
                 </div>
                 {/* Back card */}
-                <div className="back">
+                <div className="back" ref={backRef}>
                     Answer:- {flashcard.answer}
                 </div>
             </div>
