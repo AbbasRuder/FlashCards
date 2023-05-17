38a98277c5615f519b679a1b8feba68f083a9d9c
AbbasRuder <59762007+AbbasRuder@users.noreply.github.com>
2023-05-17 19:01:45 +0530

    FormFeature - Render cards according to form input

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,22 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import FormNav from './Components/FormNav'
-import FlashCardList from './Components/FlashCardList'
 import axios from 'axios'
 
 function App() {
-  const[flashCards, setFlashCards] = useState(questionAndAnswers)
 
-  useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=10').then(response => {
-      const data = response.data.results.map((list, index) => {
-        return {
-          id: `${index} ${Date.now()}`,
-          question: decodeString(list.question),
-          answer: decodeString(list.correct_answer),
-          options: [...list.incorrect_answers, list.correct_answer]
-        }
-      })
-      
-      // Sorts the 'options' in a random manner so that the correct answer is distributed randomly in the 'options'.
-      // Also decoded 'options' of any html encoded characters.
-      const sortedData = data.map(data => {
-        data.options.sort(() => Math.random() - 0.5)
-        return {
-          ...data,
-          options: data.options.map(option => decodeString(option))
-        }
-      })
-      
-      setFlashCards(sortedData)
-    })
-  }, [])
 
-  // Decodes a string from any HTML encoded characters to normal string text.
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
-  }
+  // useEffect(() => {
+   
+  // }, [])
+
+
 
   return (
     <>
       <FormNav />
-      <div className='container'>
-        <FlashCardList flashcards = {flashCards} />
-      </div>
     </>
   )
 }
 
-const questionAndAnswers = [
-  {
-    id: 1,
-    question: "What is 2 + 2 ?",
-    answer: "4",
-    options: [
-    '1',
-    '2',
-    '3',
-    '4'
-    ]
-  },
-
-  {
-    id: 2,
-    question: "What is 4 * 4 ?",
-    answer: "16",
-    options: [
-    'Answer 1',
-    'Answer 2',
-    'Answer 3',
-    'Answer 4'
-    ]
-  },
-]
 export default App
--- src/Components/FormNav.jsx
+++ src/Components/FormNav.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect, useRef } from "react";
+import FlashCardList from './FlashCardList'
 import axios from "axios";
 import "./Form.css"
 
 export default function FormNav() {
 
+    const[flashCards, setFlashCards] = useState([])
     const [categories, setCategories] = useState([])
     
     const categoryRef = useRef();
@@ -19,28 +21,68 @@ export default function FormNav() {
     
     const handleSubmit = (e) => {
         e.preventDefault();
+        axios.get('https://opentdb.com/api.php', {
+            params: {
+                category: categoryRef.current.value,
+                amount: amountRef.current.value
+            }
+        })
+        .then(response => {
+            const data = response.data.results.map((list, index) => {
+                return {
+                    id: `${index} ${Date.now()}`,
+                    question: decodeString(list.question),
+                    answer: decodeString(list.correct_answer),
+                    options: [...list.incorrect_answers, list.correct_answer]
+                }
+            })
+      
+            // Sorts the 'options' in a random manner so that the correct answer is distributed randomly in the 'options'.
+            // Also decoded 'options' of any html encoded characters.
+            const sortedData = data.map(data => {
+                data.options.sort(() => Math.random() - 0.5)
+                return {
+                ...data,
+                options: data.options.map(option => decodeString(option))
+                }
+            })
+            
+            setFlashCards(sortedData)
+        })
+    }
+
+    // Decodes a string from any HTML encoded characters to normal string text.
+    function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
     }
 
 
     return (
-        <form className="header" onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label htmlFor="category">Category</label>
-                <select id="category" ref={categoryRef}>
-                    {categories.map(category => {
-                        return <option value={category.id} key={category.id}>
-                                    {category.name}
-                                </option>
-                    })}
-                </select>
+        <>
+            <form className="header" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="category">Category</label>
+                    <select id="category" ref={categoryRef}>
+                        {categories.map(category => {
+                            return <option value={category.id} key={category.id}>
+                                        {category.name}
+                                    </option>
+                        })}
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="amount">Number of Questions</label>
+                    <input type="number" id="amount" min={1} step={1} defaultValue={10} ref={amountRef}></input>
+                </div>
+                <div className="form-group">
+                    <button className="btn">Generate</button>
+                </div>
+            </form>
+            <div className='container'>
+                <FlashCardList flashcards = {flashCards} />
             </div>
-            <div className="form-group">
-                <label htmlFor="amount">Number of Questions</label>
-                <input type="number" id="amount" min={1} step={1} defaultValue={10} ref={amountRef}></input>
-            </div>
-            <div className="form-group">
-                <button className="btn">Generate</button>
-            </div>
-        </form>
+        </>
     )
 }
