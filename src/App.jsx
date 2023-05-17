cc30abf33279f83abb9b396aeca701db84dc869f
AbbasRuder <59762007+AbbasRuder@users.noreply.github.com>
2023-05-18 00:33:52 +0530

    Seperated formNav functionality & made code clean.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import FormNav from './Components/FormNav'
-import axios from 'axios'
+
 
 function App() {
-
-
-  // useEffect(() => {
-   
-  // }, [])
-
 
 
   return (
--- src/Components/FormNav.jsx
+++ src/Components/FormNav.jsx
@@ -3,61 +3,81 @@ import FlashCardList from './FlashCardList'
 import axios from "axios";
 import "./Form.css"
 
+
+// Fetching categories data.
+async function fetchCategies() {
+    try {
+        const response = await axios.get('https://opentdb.com/api_category.php');
+        return response.data.trivia_categories
+    } catch (error) {
+        console.log('Error fetching Categories', error);
+        return [];
+    }
+}
+
+// Fetching FlashCards data according to the user input of 'categories' and 'amount'.
+async function fetchFlashCards(categories, amount) {
+    try {
+        const responseData = await axios.get('https://opentdb.com/api.php',{
+            params: {
+                categories,
+                amount
+            }
+        });
+
+        return responseData.data.results.map((list, index) => {
+            return {
+                id: `${index} ${Date.now()}`,
+                question: decodeString(list.question),
+                answer: decodeString(list.correct_answer),
+                options: [...list.incorrect_answers, list.correct_answer]
+                .map(option => decodeString(option))
+                .sort(() => Math.random() - 0.5)
+                // sort the options randomly so that every answers doesn't contain the same distribution of right answer/at the same place.
+            }
+        })
+
+    } catch(error) {
+        console.log("Error fetching flashcards", error);
+        return [];
+    }
+}
+
+// Decodes a string from any HTML encoded characters to normal string text.
+function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+}
+
+
+
 export default function FormNav() {
 
-    const[flashCards, setFlashCards] = useState([])
+    const [flashCards, setFlashCards] = useState([])
     const [categories, setCategories] = useState([])
     
     const categoryRef = useRef();
     const amountRef = useRef()
 
     useEffect(() => {
-        axios.get('https://opentdb.com/api_category.php').then(res => {
-            const categoryData = res.data.trivia_categories
-            setCategories(categoryData)
+        // resolving the promise
+        fetchCategies().then(res => {
+            setCategories(res)
         })
-        
+
     }, [])
     
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        axios.get('https://opentdb.com/api.php', {
-            params: {
-                category: categoryRef.current.value,
-                amount: amountRef.current.value
-            }
-        })
-        .then(response => {
-            const data = response.data.results.map((list, index) => {
-                return {
-                    id: `${index} ${Date.now()}`,
-                    question: decodeString(list.question),
-                    answer: decodeString(list.correct_answer),
-                    options: [...list.incorrect_answers, list.correct_answer]
-                }
-            })
-      
-            // Sorts the 'options' in a random manner so that the correct answer is distributed randomly in the 'options'.
-            // Also decoded 'options' of any html encoded characters.
-            const sortedData = data.map(data => {
-                data.options.sort(() => Math.random() - 0.5)
-                return {
-                ...data,
-                options: data.options.map(option => decodeString(option))
-                }
-            })
-            
-            setFlashCards(sortedData)
-        })
-    }
 
-    // Decodes a string from any HTML encoded characters to normal string text.
-    function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
-    }
+        // defining category and amount from user input
+        const category = categoryRef.current.value;
+        const amount = amountRef.current.value;
 
+        const fetchedFlashCards = await fetchFlashCards(category, amount);
+        setFlashCards(fetchedFlashCards);
+    }
 
     return (
         <>
